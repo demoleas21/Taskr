cb0a986703d64166e41e5fe7572fff65d3431a5a
demoleas21 <andrew.demoleas@gmail.com>
2016-06-05 16:42:40 -0400

    Tasks view added

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 import sqlite3
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 
 app = Flask(__name__)
 app.config.from_object('_config')
@@ -18,6 +18,7 @@ def login_required(test):
         else:
             flash('You need to login first.')
             return redirect(url_for('login'))
+
     return wrap
 
 
@@ -29,6 +30,7 @@ def logout():
 
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
@@ -39,3 +41,26 @@ def login():
             flash('Welcome!')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+
+    cur = g.db.execute('SELECT name, due_date, priority, task_id FROM tasks WHERE status=1')
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3])
+        for row in cur.fetchall()
+        ]
+
+    cur = g.db.execute('SELECT name, due_date, priority, task_id FROM tasks WHERE status=0')
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3])
+        for row in cur.fetchall()
+        ]
+
+    g.db.close()
+    return render_template('tasks.html', form=AddTaskForm(request.form),
+                           open_tasks=open_tasks, closed_tasks=closed_tasks
+                           )
