3331ee70173f2fd12f35f78e7fd8f756347c8a12
demoleas21 <andrew.demoleas@gmail.com>
2016-06-07 22:58:52 -0400

    Refactored using SQLAlchemy

--- src/db_create.py
+++ src/db_create.py
@@ -1,20 +1,8 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    c = connection.cursor()
-
-    c.execute("""
-      CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,
-      due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)
-    """)
-
-    c.execute("""
-      INSERT INTO tasks(name, due_date, priority, status)
-      VALUES('Finish this tutorial', '03/25/2015', 10, 1)
-    """)
-
-    c.execute("""
-      INSERT INTO tasks(name, due_date, priority, status)
-      VALUES('Finish Real Python Course 2', '03/25/2015', 10, 1)
-    """)
+db.create_all()
+db.session.add(Task("Finish this tutorial", date(2016, 6, 30), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 8, 31), 10, 1))
+db.session.commit()
