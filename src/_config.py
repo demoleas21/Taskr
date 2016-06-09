712c17a19523de4bbb6237256070b3ec6cfd147f
demoleas21 <andrew.demoleas@gmail.com>
2016-06-08 22:48:01 -0400

    Added user registration support

--- src/_config.py
+++ src/_config.py
@@ -3,8 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x93v\xdc\xb9\x8f\xd6a\x12/\xff\x12LM\xe8\x13\xf5\xe2\xbe\x84\xc0G\xe5`\xa6'
 
