5129e531f63ecdc6f34c832acd67715db284088c
demoleas21 <andrew.demoleas@gmail.com>
2016-06-14 23:18:23 -0400

    Created test file

---
+++ src/test.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+from views import app, db
+from _config import basedir
+from models import User
+
+TEST_DB = 'test.db'
+
+
+class AllTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATEBASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    @staticmethod
+    def test_user_setup():
+        new_user = User('andrew', 'andrew@ademoleas.org', 'andrewdemoleas')
+        db.session.add(new_user)
+        db.session.commit()
+
+if __name__ == "__main__":
+    unittest.main()
