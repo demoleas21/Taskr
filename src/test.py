c7ab8357d09364bbf2e13f7107875f7c4836e86d
demoleas21 <andrew.demoleas@gmail.com>
2016-08-05 08:22:46 -0400

    Added tests to check for users ability to view tasks page. All tests passing now!

--- src/test.py
+++ src/test.py
@@ -26,13 +26,16 @@ class AllTests(unittest.TestCase):
         ), follow_redirects=True)
 
     def register(self, name, email, password, confirm):
-        return self.app.post('/register', data=dict(
+        return self.app.post('register/', data=dict(
             name=name, email=email, password=password, confirm=confirm
         ), follow_redirects=True)
 
+    def logout(self):
+        return self.app.get('logout/', follow_redirects=True)
+
     @staticmethod
     def test_users_can_register():
-        new_user = User('andrew', 'andrew@taskr.com', '1234')
+        new_user = User('andrew', 'andrew@taskr.com', '123456')
         db.session.add(new_user)
         db.session.commit()
         test = db.session.query(User).all()
@@ -50,21 +53,42 @@ class AllTests(unittest.TestCase):
         self.assertIn(b'Invalid username or password.', response.data)
 
     def test_users_can_login(self):
-        self.register('Andrew', 'andrew@taskr.com', '1234', '1234')
-        response = self.login('Andrew', '1234')
+        self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
+        response = self.login('Andrew', '123456')
         self.assertIn(b'Welcome!', response.data)
 
     def test_invalid_form_data(self):
-        self.register('Andrew', 'andrew@taskr.com', '1234', '1234')
+        self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
         response = self.login('alert("alert box!");', 'foo')
         self.assertIn(b'Invalid username or password.', response.data)
 
     def test_user_registration_error(self):
         self.app.get('register/', follow_redirects=True)
-        self.register('Andrew', 'andrew@taskr.com', '1234', '1234')
+        self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
         self.app.get('register/', follow_redirects=True)
-        response = self.register('Andrew', 'andrew@taskr.com', '1234', '1234')
-        self.assertIn(b'That Username and/or email already exist.', response.data)
+        response = self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
+        self.assertIn(b'That username and/or email already exists.', response.data)
+
+    def test_logged_in_users_can_logout(self):
+        self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
+        self.login('Andrew', '123456')
+        response = self.logout()
+        self.assertIn(b'Goodbye!', response.data)
+
+    def test_not_logged_in_users_cannot_logout(self):
+        response = self.logout()
+        self.assertNotIn(b'Goodbye!', response.data)
+
+    def test_logged_in_users_can_access_tasks_page(self):
+        self.register('Andrew', 'andrew@taskr.com', '123456', '123456')
+        self.login('Andrew', '123456')
+        response = self.app.get('tasks/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Add a new task:', response.data)
+
+    def test_not_logged_in_users_cannot_access_tasks_page(self):
+        response = self.app.get('tasks/', follow_redirects=True)
+        self.assertIn(b'You need to login first.', response.data)
 
 
 if __name__ == "__main__":
