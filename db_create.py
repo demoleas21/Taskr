ad06eb0e4bd8a09f33d457a97908543f440c46b7
demoleas21 <andrew.demoleas@gmail.com>
2016-08-12 08:59:01 -0400

    Updated db creation to create admin user

--- db_create.py
+++ db_create.py
@@ -1,8 +1,9 @@
-from views import db
-from models import Task
+from src import db
+from src.models import Task, User
 from datetime import date, datetime
 
 db.create_all()
-db.session.add(Task("Finish this tutorial", date(2016, 6, 30), 10, datetime.utcnow(), 1, 0))
-db.session.add(Task("Finish Real Python", date(2016, 8, 31), 10, datetime.utcnow(), 1, 0))
+db.session.add(User('Administrator', 'admin@taskr.com', '123456', 'admin'))
+db.session.add(Task('Finish this website', date(2016, 6, 30), 10, datetime.utcnow(), 1, 1))
+db.session.add(Task('Finish iOS application', date(2016, 8, 31), 10, datetime.utcnow(), 1, 1))
 db.session.commit()
